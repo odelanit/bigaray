07b32c095a02d11c7bb5ca702b5f513169260e0c
jinri1218 <jinri.1218@yahoo.com>
2020-11-03 03:48:28 +0800

    added celery

--- bigaray/settings.py
+++ bigaray/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
+    'django_celery_beat',
     'scraping'
 ]
 
@@ -205,3 +206,11 @@ LOGGING = {
         }
     }
 }
+
+# CELERY
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'America/Toronto'
