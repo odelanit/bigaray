a55341ae0a834b01a33662c5f7acf8f24d54fd7d
jinri1218 <hesimiao2019@gmail.com>
2020-11-06 21:45:43 +0800

    fixed lole

--- uploads/spiders/lole_1_1.py
+++ uploads/spiders/lole_1_1.py
@@ -8,7 +8,7 @@ class ProductSpider(scrapy.Spider):
     allowed_domains = ['www.lolelife.com']
     base_url = 'https://www.lolelife.com'
     start_urls = [
-        'https://www.lolelife.com/women/new-arrivals?page=%s' % page for page in range(1, 2)
+        'https://www.lolelife.com/women/new-arrivals?page=%s' % page for page in range(1, 9)
     ]
 
     custom_settings = {
