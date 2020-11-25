2962db68f1d9e6d2f9d768a42c164ac90d2ba36b
fullstack0220 <fullstack0220@outlook.com>
2020-11-25 11:28:46 +0800

    added run_spiders.py

--- scraping/management/commands/run_spiders.py
+++ scraping/management/commands/run_spiders.py
@@ -4,8 +4,9 @@ from scraping.models import Scraper
 
 
 class Command(BaseCommand):
-    help = "crawl Hm_1_1"
+    help = "run all scrapers"
 
     def handle(self, *args, **options):
-        scraper = Scraper.objects.get(id=54)
-        scraper.start()
+        scrapers = Scraper.objects.all()
+        for scraper in scrapers:
+            scraper.start()
